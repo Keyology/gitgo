48e742174c3de4be7ecb4a31970eb61d81fdf948
Keoni Murray <keonimurray45@gmail.com>
2019-03-05 16:46:33 -0800

    made api connection

--- user.go
+++ user.go
@@ -2,65 +2,96 @@ package main
 
 import (
 	"encoding/json"
-	"time"
+	"fmt"
+
+	// "time"
 	// "fmt"
-	"io/ioutil"
+	// "io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 const (
-	apiURL       = "https://api.github.com"
-	userEndpoint = "/users/"
+	apiURL = "https://api.microlink.io?url="
 )
 
-type User struct {
-	Login             string      `json:"login"`
-	ID                int         `json:"id"`
-	AvatarURL         string      `json:"avatar_url"`
-	GravatarID        string      `json:"gravatar_id"`
-	URL               string      `json:"url"`
-	HTMLURL           string      `json:"html_url"`
-	FollowersURL      string      `json:"followers_url"`
-	FollowingURL      string      `json:"following_url"`
-	GistsURL          string      `json:"gists_url"`
-	StarredURL        string      `json:"starred_url"`
-	SubscriptionsURL  string      `json:"subscriptions_url"`
-	OrganizationsURL  string      `json:"organizations_url"`
-	ReposURL          string      `json:"repos_url"`
-	EventsURL         string      `json:"events_url"`
-	ReceivedEventsURL string      `json:"received_events_url"`
-	Type              string      `json:"type"`
-	SiteAdmin         bool        `json:"site_admin"`
-	Name              string      `json:"name"`
-	Company           string      `json:"company"`
-	Blog              string      `json:"blog"`
-	Location          string      `json:"location"`
-	Email             string      `json:"email"`
-	Hireable          interface{} `json:"hireable"`
-	Bio               string      `json:"bio"`
-	PublicRepos       int         `json:"public_repos"`
-	PublicGists       int         `json:"public_gists"`
-	Followers         int         `json:"followers"`
-	Following         int         `json:"following"`
-	CreatedAt         time.Time   `json:"created_at"`
-	UpdatedAt         time.Time   `json:"updated_at"`
+
+func GetLink() {
+	newData := "https://www.youtube.com/watch?v=x4Xt3P7FQ2M&t=924s"
+	var payload Schema
+
+	resp, err := http.Get(apiURL + newData)
+
+	if err != nil {
+
+		log.Fatalf("Error retrieving data: %s\n", err)
+
+	}
+
+	json.NewDecoder(resp.Body).Decode(&payload)
+	json.NewEncoder(os.Stdout).Encode(payload)
+	fmt.Println("This is before close", payload)
+
+	resp.Body.Close()
+	fmt.Println("This is after close", payload)
+
+}
+func main(){
+	GetLink()
+}
+// func getUsers(name string) User {
+// 	resp, err := http.Get(apiURL + userEndpoint + name)
+// 	if err != nil {
+// 		log.Fatalf("Error retrieving data: %s\n", err)
+// 	}
+
+// 	defer resp.Body.Close()
+
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		log.Fatalf("Error reading data: %s\n", err)
+// 	}
+
+// 	var user User
+// 	json.Unmarshal(body, &user)
+// 	return user
+// }
+
+// Microlink API follows JSend schema format
+// See more: https://labs.omniti.com/labs/jsend
+
+type Schema struct {
+	Status  string `json:"status"`
+	Message string `json:"message,omitempty"`
+	Data    *Data  `json:"data,omitempty"`
 }
 
-func getUsers(name string) User {
-	resp, err := http.Get(apiURL + userEndpoint + name)
-	if err != nil {
-		log.Fatalf("Error retrieving data: %s\n", err)
-	}
+type Data struct {
+	Author      string    `json:"author,omitempty"`
+	Lang        string    `json:"lang,omitempty"`
+	Date        string    `json:"date,omitempty"`
+	Title       string    `json:"title,omitempty"`
+	Description string    `json:"description,omitempty"`
+	Publisher   string    `json:"publisher,omitempty"`
+	URL         string    `json:"url"`
+	Image       *ImageURL `json:"image,omitempty"`
+	Video       *ImageURL `json:"video,omitempty"`
+	Logo        *ImageURL `json:"logo,omitempty"`
+	Screenshot  *ImageURL `json:"screenshot,omitempty"`
+}
 
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error reading data: %s\n", err)
-	}
-	
-	var user User
-	json.Unmarshal(body, &user)
-	return user
+type ImageURL struct {
+	Width            int      `json:"width,omitempty"`
+	Height           int      `json:"height,omitempty"`
+	Size             int      `json:"size,omitempty"`
+	SizePretty       string   `json:"size_pretty,omitempty"`
+	Duration         int      `json:"duration,omitempty"`
+	DurationPretty   string   `json:"duration_pretty,omitempty"`
+	Type             string   `json:"type,omitempty"`
+	URL              string   `json:"url"`
+	Palette          []string `json:"palette,omitempty"`
+	BackgroundColor  string   `json:"background_color,omitempty"`
+	AlternativeColor string   `json:"alternative_color,omitempty"`
+	Color            string   `json:"color,omitempty"`
 }
